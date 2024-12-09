038971bc99457ec890905131469b568e8a985715
mikelezc <lezcano.bassist@gmail.com>
2024-12-09 21:46:48 +0100

    42 from api borrado (no hacía nada)

--- srcs/django/authentication/api/views/__init__.py
+++ srcs/django/authentication/api/views/__init__.py
@@ -9,7 +9,6 @@ from .profile_views import *
 from .password_views import *
 from .gdpr_views import *
 from .verification_views import *
-from .fortytwo_views import *
 
 __all__ = [
     'LoginAPIView',
