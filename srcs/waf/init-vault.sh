78315d0bef1aa05a1909f716c0eb776fa54c385c
mikelezc <lezcano.bassist@gmail.com>
2024-12-17 20:10:53 +0100

    waf dockerfile fixed 95%

--- srcs/waf/init-vault.sh
+++ srcs/waf/init-vault.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-# Inicializar Vault
-vault operator init > /etc/vault.d/init.txt
+# Iniciar Vault en modo desarrollo
+vault server -dev -dev-root-token-id=myroot -dev-listen-address=0.0.0.0:8200 &
 
-# Desbloquear Vault
-vault operator unseal $(grep 'Key 1:' /etc/vault.d/init.txt | awk '{print $NF}')
-vault operator unseal $(grep 'Key 2:' /etc/vault.d/init.txt | awk '{print $NF}')
-vault operator unseal $(grep 'Key 3:' /etc/vault.d/init.txt | awk '{print $NF}')
+# Esperar a que Vault esté listo
+sleep 5
 
-# Login
-vault login $(grep 'Initial Root Token:' /etc/vault.d/init.txt | awk '{print $NF}')
+export VAULT_ADDR='http://127.0.0.1:8200'
+export VAULT_TOKEN=myroot
 
 # Habilitar secrets engine
 vault secrets enable -path=secret kv-v2
@@ -24,15 +22,49 @@ EOF
 # Almacenar secretos iniciales
 vault kv put secret/django/config \
     SECRET_KEY="tu_django_secret_key" \
-    DB_PASSWORD="tu_db_password" \
-    OAUTH_CLIENT_ID="tu_oauth_id" \
-    OAUTH_CLIENT_SECRET="tu_oauth_secret"
+    DB_PASSWORD="tu_db_password"
 
 # Habilitar auditoría
 vault audit enable file file_path=/var/log/vault/audit.log
 
-# Verificar estado
-vault status
+# Mantener nginx en segundo plano
+nginx -g 'daemon off;'
 
-# Mantener el proceso en ejecución
-exec nginx -g 'daemon off;'
+# #!/bin/sh
+
+# # Inicializar Vault
+# vault operator init > /etc/vault.d/init.txt
+
+# # Desbloquear Vault
+# vault operator unseal $(grep 'Key 1:' /etc/vault.d/init.txt | awk '{print $NF}')
+# vault operator unseal $(grep 'Key 2:' /etc/vault.d/init.txt | awk '{print $NF}')
+# vault operator unseal $(grep 'Key 3:' /etc/vault.d/init.txt | awk '{print $NF}')
+
+# # Login
+# vault login $(grep 'Initial Root Token:' /etc/vault.d/init.txt | awk '{print $NF}')
+
+# # Habilitar secrets engine
+# vault secrets enable -path=secret kv-v2
+
+# # Crear política para Django
+# vault policy write django - <<EOF
+# path "secret/data/django/*" {
+#   capabilities = ["create", "read", "update", "delete", "list"]
+# }
+# EOF
+
+# # Almacenar secretos iniciales
+# vault kv put secret/django/config \
+#     SECRET_KEY="tu_django_secret_key" \
+#     DB_PASSWORD="tu_db_password" \
+#     OAUTH_CLIENT_ID="tu_oauth_id" \
+#     OAUTH_CLIENT_SECRET="tu_oauth_secret"
+
+# # Habilitar auditoría
+# vault audit enable file file_path=/var/log/vault/audit.log
+
+# # Verificar estado
+# vault status
+
+# # Mantener el proceso en ejecución
+# exec nginx -g 'daemon off;'
