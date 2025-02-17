49fe1867408dc16e3882be7aab10768ea245d3ad
mikelezc <138023008+mikelezc@users.noreply.github.com>
2025-02-17 20:34:01 +0100

    db optimizada

--- srcs/db/config/init-db.sh
+++ srcs/db/config/init-db.sh
@@ -6,6 +6,15 @@ if [ ! -f "${PGDATA}/postgresql.conf" ]; then
     # Basic PostgreSQL configuration
     cat >> "${PGDATA}/postgresql.conf" <<EOF
 listen_addresses = '*'
+
+# Checkpoint and Performance tuning
+checkpoint_timeout = 300
+checkpoint_completion_target = 0.9
+checkpoint_warning = 30s
+max_wal_size = 1GB
+min_wal_size = 80MB
+effective_cache_size = 1GB
+maintenance_work_mem = 128MB
 EOF
 
     # Copy pg_hba.conf
