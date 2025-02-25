4fe686725b06d5c4fb51590d66cb2b218cac7c0c
mikelezc <138023008+mikelezc@users.noreply.github.com>
2025-02-25 20:04:02 +0100

    test OK arreglado? GDPRService.cleanup_inactive_users()

--- srcs/django/authentication/tasks.py
+++ srcs/django/authentication/tasks.py
@@ -27,3 +27,12 @@ CELERY_BEAT_SCHEDULE = {
         'schedule': settings.TASK_CHECK_INTERVAL,
     },
 }
+
+    
+# Tasks are used to perform background operations in Django. 
+# They are defined as functions and are executed by the Celery worker. 
+# In this case, the cleanup_inactive_users task is used to remove users who have been inactive for more than 60 days. 
+# The task is scheduled to run daily through the CELERY_BEAT_SCHEDULE setting in settings.py. 
+# The task calls the cleanup_inactive_users method from the GDPRService class, which handles the deletion of inactive users. 
+# The task logs information about the cleanup process and any errors that occur. 
+# If an error occurs, it is logged and raised to the caller.
